667f9ff964b528343111f6852ae54aaafdef26ac
jonthefreelance <jonthefreelance@gmail.com>
2024-08-24 23:45:47 +0800

    add records to Products

--- ContosoPizzza/Program.cs
+++ ContosoPizzza/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ContosoPizzza.Data;
+using ContosoPizzza.Models;
+
+using ContosoPizzaContext context = new ContosoPizzaContext();
+
+Product veggieSpecial = new Product()
+{
+    Name = "Veggie Special Pizza",
+    Price = 9.99M
+};
+
+context.Products.Add(veggieSpecial);
+
+Product deluxeMeat = new Product()
+{
+    Name = "Deluxe Meat Pizza",
+    Price = 12.99M
+};
+
+context.Add(deluxeMeat);
+context.SaveChanges();
+
