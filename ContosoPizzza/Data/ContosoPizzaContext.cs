eb6cf78ede52ec5748c38326645eaeb3b63bfd24
jonthefreelance <jonthefreelance@gmail.com>
2024-08-24 23:20:23 +0800

    Models and Context

---
+++ ContosoPizzza/Data/ContosoPizzaContext.cs
@@ -0,0 +1,23 @@
+﻿using ContosoPizzza.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContosoPizzza.Data
+{
+    public class ContosoPizzaContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"");
+        }
+    }
+}
